a947afc1c4e425d602df7f3e76559c267b9c8994
Alexandra Dinca <alexandradi2003@gmail.com>
2025-11-09 16:22:59 +0200

    Added lab05 - finished

---
+++ Laborator05/javaSkelStartedService/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
@@ -0,0 +1,84 @@
+package ro.pub.cs.systems.eim.lab05.startedservice.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import ro.pub.cs.systems.eim.lab05.startedservice.general.Constants;
+
+public class StartedService extends Service {
+
+    private static final String TAG = "ForegroundService";
+    private static final String CHANNEL_ID = "11";
+    private static final String CHANNEL_NAME = "ForegroundServiceChannel";
+    private void dummyNotification() {
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH);
+        }
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(channel);
+        }
+
+        Notification notification = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(getApplicationContext(),CHANNEL_ID).build();
+        }
+
+        startForeground(1, notification);
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(Constants.TAG, "onCreate() method was invoked");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(Constants.TAG, "onBind() method was invoked");
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(Constants.TAG, "onUnbind() method was invoked");
+        return true;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d(Constants.TAG, "onRebind() method was invoked");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(Constants.TAG, "onDestroy() method was invoked");
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(Constants.TAG, "onStartCommand() method was invoked");
+
+        //  Notify
+        dummyNotification();
+
+        //  Start thread
+        Thread thread = new ProcessingThread(this);
+        thread.start();
+
+        return START_REDELIVER_INTENT;
+    }
+
+}
